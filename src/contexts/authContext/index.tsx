e0c2a6f7d499cee0e1056354cd6bcfef4c60fc7f
Ardie Dela Cruz <cs.ardiezdelacruz@gmail.com>
2025-08-14 13:39:23 +0800

    fixed role data type

--- src/contexts/authContext/index.tsx
+++ src/contexts/authContext/index.tsx
@@ -40,7 +40,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
             aid: decoded.aid,
             email: decoded.email,
             name: decoded.name,
-            role: decoded.role,
+            role: [decoded.role],
           });
           setLoading(false);
         }
@@ -70,7 +70,10 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
       const { token, user } = await res.json();
 
       setToken(token); 
-      setUser(user);               
+      setUser({
+        ...user,
+        role: Array.isArray(user.role) ? user.role : [user.role],
+      });              
       localStorage.setItem("token", token);
       navigate("/");
       return user;
@@ -96,7 +99,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
     loading,
     userLoggedIn: !!user,
     userName: user?.name ?? null,
-    userRole: user?.role ?? [],
+    userRole: Array.isArray(user?.role) ? user.role : [],
   };
 
   return (
